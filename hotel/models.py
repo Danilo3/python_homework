0ed1d223fabd0cfd43f96b487317c04b06088c38
Danilo3 <danil.2010846@yandex.ru>
2018-01-15 01:29:22 +0300

    add models Room and Facilty

--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class RoomFacility(models.Model):
+    name = models.CharField(max_length=20)
+    icon_number = models.IntegerField(default=0)
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(max_length=300)
+    price = models.IntegerField(default=0)
+    amount = models.IntegerField()
+    image_path = models.CharField(max_length=100, default="")
+    facilities = models.ManyToManyField(RoomFacility)
+
+
--- hotel/views.py
+++ hotel/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import generic
 
+from hotel.models import Room
+
 
 class IndexView(generic.TemplateView):
     template_name = 'hotel/index.html'
 
 
-class RoomsView(generic.TemplateView):
+class RoomsView(generic.ListView):
     template_name = 'hotel/rooms.html'
+    context_object_name = 'all_rooms_list'
+
+    def get_queryset(self):
+        return Room.objects.all()
