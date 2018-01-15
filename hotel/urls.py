35f84114d05722fb02cce2edaaca147230638d7f
Danilo3 <danil.2010846@yandex.ru>
2018-01-15 19:45:07 +0300

    simple registration

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,10 +1,11 @@
 from . import views
-from django.urls import path
-
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('rooms/', views.RoomsView.as_view(), name='rooms'),
     path('room/<int:pk>/', views.RoomDetail.as_view(), name ='room'),
-    path('contacts', views.contactsView, name ="contact")
+    path('contacts', views.contactsView, name="contact"),
+    path('login/', views.user_login, name="login"),
+    path('register/', views.RegisterFormView.as_view(), name ="register" )
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -1,8 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+
+# Опять же, спасибо django за готовую форму аутентификации.
+from django.contrib.auth.forms import AuthenticationForm
+
+# Функция для установки сессионного ключа.
+# По нему django будет определять, выполнил ли вход пользователь.
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 from django.views import generic
 
+from .forms import LoginForm
 from hotel.models import Room
 
 
@@ -22,5 +34,63 @@ class RoomDetail(generic.DetailView):
     model = Room
     template_name = 'hotel/room.html'
 
+
 def contactsView(request):
     return render(request, 'hotel/contacts.html')
+
+
+class RegisterFormView(FormView):
+    form_class = UserCreationForm
+
+    # Ссылка, на которую будет перенаправляться пользователь в случае успешной регистрации.
+    # В данном случае указана ссылка на страницу входа для зарегистрированных пользователей.
+    success_url = "/login/"
+
+    # Шаблон, который будет использоваться при отображении представления.
+    template_name = "hotel/register.html"
+
+    def form_valid(self, form):
+        # Создаём пользователя, если данные в форму были введены корректно.
+        form.save()
+
+        # Вызываем метод базового класса
+        return super(RegisterFormView, self).form_valid(form)
+
+
+
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+
+    # Аналогично регистрации, только используем шаблон аутентификации.
+    template_name = "hotel/login.html"
+
+    # В случае успеха перенаправим на главную.
+    success_url = "/"
+
+    def form_valid(self, form):
+        # Получаем объект пользователя на основе введённых в форму данных.
+        self.user = form.get_user()
+
+        # Выполняем аутентификацию пользователя.
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'hotel/login.html', {'form': form})
