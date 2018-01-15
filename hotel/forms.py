35b2b1c7adb6fe92849613fa4e1ce0fdacb630dc
Danilo3 <danil.2010846@yandex.ru>
2018-01-15 23:25:20 +0300

    add registration

--- hotel/forms.py
+++ hotel/forms.py
@@ -6,14 +6,14 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 
-
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password = forms.CharField(label='Password',min_length=8, widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    # username = forms.CharField(label='Username', min_length=5)
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('username','first_name', 'last_name', 'email')
 
     def clean_password2(self):
         cd = self.cleaned_data
